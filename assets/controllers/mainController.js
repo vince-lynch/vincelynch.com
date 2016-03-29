61bff1c2b1f690e617e72c828d8eccb667f90d8b
error25 <autovincelynch@gmail.com>
2016-03-29 15:17:29 +0100

    made a timeline

---
+++ assets/controllers/mainController.js
@@ -0,0 +1,13 @@
+
+
+angular.module('VinceLynch')
+.controller('MainController', MainController);
+
+function MainController() {
+
+self = this;
+console.log ("angular controller loaded")
+
+this.helloworld = "Hello World";
+
+}
