fd08b3b6420a52ad4d9cef4681aef4ffd43df115
error25 <autovincelynch@gmail.com>
2016-03-30 21:43:05 +0100

    some crazy idea about animated gifs on the fly from mobile phone

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,26 @@
 $( document ).ready(function() {
-    console.log( "jquery ready!" );
+    console.log( "PLAY with GIFshot ready!" );
+
+    
+
+        gifshot.createGIF({
+
+          // Desired width of the image 
+          'gifWidth': 400,
+          // Desired height of the image 
+          'gifHeight': 400,
+
+
+        }, function(obj) {
+          if(!obj.error) {
+
+            var image = obj.image,
+            animatedImage = document.createElement('img');
+            animatedImage.src = image;
+            document.body.appendChild(animatedImage);
+          }
+        });
+
+  
+
 });
