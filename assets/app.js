6d5dfdd0457259afa471b927c52fcecfd44eada1
error25 <autovincelynch@gmail.com>
2016-04-11 08:54:48 +0100

    getting back to the blog

--- assets/app.js
+++ assets/app.js
@@ -1,25 +1,9 @@
 $( document ).ready(function() {
-    console.log( "PLAY with GIFshot ready!" );
+    console.log( "jQuery Loaded" );
 
     
 
-        gifshot.createGIF({
 
-          // Desired width of the image 
-          'gifWidth': 400,
-          // Desired height of the image 
-          'gifHeight': 400,
-
-
-        }, function(obj) {
-          if(!obj.error) {
-
-            var image = obj.image,
-            animatedImage = document.createElement('img');
-            animatedImage.src = image;
-            document.body.appendChild(animatedImage);
-          }
-        });
 
   
 
