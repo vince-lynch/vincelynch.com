fbd19a16e008f416ef3acc386a421f9241dec806
error25 <autovincelynch@gmail.com>
2016-06-15 20:04:34 +0100

    going to put the server back up with the user model and routes in place, I need to install JWT, and bcrypt modules on the server, then maybe I'll be able to move onto the frontend

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,24 @@
 var Feed = require('./models/feed');
+var User = require('../models/user');
+var jwt = require('jsonwebtoken');
+var secret = require('../config/tokens').secret;
+
+// custom JWT middleware
+function secureRoute(req, res, next) {
+  if(!req.headers.authorization) return res.status(401).json({ message: 'Unauthorized' });
+
+  var token = req.headers.authorization.replace('Bearer ', '');
+
+  jwt.verify(token, secret, function(err, user) {
+    if(!user) return res.status(401).json({ message: 'Invalid token' });
+    req.user = user;
+    next();
+  });
+}
+
     module.exports = function(app) {
+
+
 
 //////////// STATICS //////////////////
         
@@ -57,5 +76,63 @@ var Feed = require('./models/feed');
             res.status(204).send("successfully deleted");
           }).select('-__v');
         });
+
+        
+    app.get('/users', function(req, res) {
+      User.find(function(err, users) {
+        if(err) return res.status(500).json({ message: err });
+        return res.status(200).json(users);
+      });
+    }
+
+    app.get('/users/:id', function(req, res) {
+      User.findById(req.params.id, function(err, user) {
+        if(err) return res.status(500).json({ message: err });
+        return res.status(200).json(user);
+      });
+    }
+
+    app.put('/users/:id', function(req, res) {
+      User.findByIdAndUpdate(req.params.id, req.body, { new: true }, function(err, user) {
+        if(err) return res.status(500).json({ message: err });
+        return res.status(200).json(user);
+      });
+    }
+
+    app.delete('/users/:id', function(req, res) {
+      User.findByIdAndRemove(req.params.id, function(err) {
+        if(err) return res.status(500).json({ message: err });
+        return res.status(204).send();
+      });
+    }
+
+    app.post('/register', function(req, res) {
+      User.create(req.body, function(err, user) {
+        // tidy up mongoose's awful error messages
+        if(err) {
+          if(err.code && (err.code === 11000 || err.code === 11001)) {
+            var attribute = err.message.match(/\$([a-z]+)_/)[1];
+            err = "An account with that " + attribute + " already exists";
+          }
+          return res.status(400).json({ message: err.toString() });
+        }
+
+        var payload = { _id: user._id, username: user.username };
+        var token = jwt.sign(payload, secret, "24h");
+        return res.status(200).json({ message: "Thanks for registering", user: user, token: token });
+      });
+    }
+
+    app.post('/login', function login(req, res) {
+      console.log(req.body)
+      User.findOne({ email: req.body.email }, function(err, user) {
+        if(err) return res.send(500).json({ message: err });
+        if(!user || !user.validatePassword(req.body.password)) return res.status(401).json({ message: "Unauthorized" });
+
+        var payload = { _id: user._id, username: user.username };
+        var token = jwt.sign(payload, secret, "24h");
+        return res.status(200).json({ message: "Login successful", user: user, token: token });
+      });
+    }
 
 };
